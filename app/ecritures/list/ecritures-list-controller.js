ff86031bcbeebc7f29517e01def4562172e376e9
François Lagoueyte <francois.lagoueyte@asso-cocktail.fr>
2014-04-01 14:16:59 +0200

    Ajout fonction pour fermer le test des alertes

--- app/ecritures/list/ecritures-list-controller.js
+++ app/ecritures/list/ecritures-list-controller.js
@@ -34,4 +34,8 @@ angular.module('interpreteurBudgetaireApp')
 	    { type: 'danger', msg: 'Oh snap! Change a few things up and try submitting again.' },
 	    { type: 'success', msg: 'Well done! You successfully read this important alert message.' }
 	  ];
+
+	$scope.closeAlert = function(index) {
+	    $scope.alerts.splice(index, 1);
+	  };
 });
