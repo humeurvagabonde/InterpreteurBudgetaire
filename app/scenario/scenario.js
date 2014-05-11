f99eefb247c701906a49f243e6e8e19e934e289d
HumeurVagabonde <francois.lagoueyte@gmail.com>
2014-05-11 19:45:57 +0200

    Design message par ligne (en cours)

--- app/scenario/scenario.js
+++ app/scenario/scenario.js
@@ -1,31 +1,44 @@
 'use strict';
 
 angular.module('interpreteurBudgetaireApp.scenario', ['ui.router', 'interpreteurBudgetaireApp.referentiel'])
-	.config(function ($stateProvider) {
+  .config(function ($stateProvider) {
     $stateProvider
-	    .state('scenario', {
-	    	abstract: true,
-	    	resolve: {
-	    		typesEvenement: function(referentielService) {
-	    			return referentielService.loadTypesEvenement();
-	    		},
-	    		lignesBudgetaires: function(referentielService) {
-	    			return referentielService.loadLignesBudgetaires();
-	    		},
-	    		axes: function(referentielService) {
-	    			return referentielService.loadAxes();
-	    		},
-	    		operations: function(referentielService) {
-	    			return referentielService.loadOperations();
-	    		}
-	    	},
-	    	url: '/scenario',
-	    	templateUrl: 'scenario/scenario.html',
-      	controller: 'ScenarioCtrl'
-		})
-		.state('scenario.creer', {
-			url: '/creer',
-			views:
-		})
+      .state('scenario', {
+        abstract: true,
+        resolve: {
+          typesEvenement: function(referentielService) {
+            return referentielService.loadTypesEvenement();
+          },
+          lignesBudgetaires: function(referentielService) {
+            return referentielService.loadLignesBudgetaires();
+          },
+          axes: function(referentielService) {
+            return referentielService.loadAxes();
+          },
+          operations: function(referentielService) {
+            return referentielService.loadOperations();
+          }
+        },
+        url: '/scenario',
+        templateUrl: 'scenario/scenario.html',
+          controller: 'ScenarioCtrl'
+    })
+    .state('scenario.creer', {
+      url: '/creer',
+      views: {
+        'referentiels-liste': {
+          templateUrl: 'scenario/scenario-referentiels-liste.html',
+          controller: 'ScenarioCtrl'
+        },
+        'referentiels-details': {
+          templateUrl: 'scenario/scenario-referentiels-details.html',
+          controller: 'ScenarioCtrl'
+        },
+        'message-composition': {
+          templateUrl: 'scenario/scenario-message-composition.html',
+          controller: 'ScenarioCtrl'
+        },
+      }
+    })
   }
 );
